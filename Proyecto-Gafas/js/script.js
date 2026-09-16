c9119908f56d6b04a86070d702bd283b66d8bf63
Teo <mateoq152006@gmail.com>
2026-09-16 12:32:28 -0500

    5 commit: corrige los metodos del comentario 4

--- Proyecto-Gafas/js/script.js
+++ Proyecto-Gafas/js/script.js
@@ -1,143 +1,184 @@
 
+// SELECCION DE ELEMENTOS DEL HTML
 
-// se abtrae los HTML para manerjarlos en js
-const titulo = document.getElementByid("titulo");
-const btnTexto = document.getElementByid("btn-texto");
-const btnEstilo = document.getElementByid("btn-estilo");
-const btnAgrgar = document.getElementByid("btn-agregar");
-const btnEliminar = document.getElementByid("btn-eliminar");
-const btnApi = document.getElementByid("btn-api");
+// Se extraen los elementos HTML para manejarlos en JS
+
+const titulo = document.getElementById("titulo");
+
+const btnTexto = document.getElementById("btn-texto");
+
+const btnEstilo = document.getElementById("btn-estilo");
+
+const btnAgregar = document.getElementById("btn-agregar");
+
+const btnEliminar = document.getElementById("btn-eliminar");
+
+const btnApi = document.getElementById("btn-api");
 
 const contenedorGafas = document.getElementById("contenedor-gafas");
+
 const infoVentana = document.getElementById("info-ventana");
+
 const resultadoApi = document.getElementById("resultado-api");
 
-// Seleccion mediante querySelector y eso es Busca el primer elemento que coincida con el texto
+
+// SELECCION MEDIANTE querySelector
+
+
+// Busca el primer elemento que coincida con el selector
+
 const descripcion = document.querySelector(".descripcion");
 
 
 
-// Seleccion mediante querySelectorAll y eso es Buscar TODOS los elementos que coincidan
+// SELECCION MEDIANTE querySelectorAll
+
+
+// Busca TODOS los elementos que coincidan con el selector
+
 const categorias = document.querySelectorAll(".categorias li");
 
 
-// se muetar lo elemento de consola paar 
-console.log("Titulo:", titulo); 
+ 
+// MOSTRAR ELEMENTOS EN CONSOLA
+
+
+console.log("Titulo:", titulo);
+
 console.log("Descripcion:", descripcion);
+
 console.log("Categorias:", categorias);
+
 console.log("Contenedor de gafas:", contenedorGafas);
 
 
-btnTexto.addEventLIstener("click", function () {
-    
-    //se hixo un aviso en termnart paar saber que se esta haciendo
+
+// CAMBIO DE TEXTO
+
+btnTexto.addEventListener("click", function () {
+
+    // Aviso para saber que se esta ejecutando el evento
+
     console.log("btnTexto - Se hizo clic en cambiar texto");
 
 
-    // se cambia el texto del titulo 
-    titulo.textContent = " Encuentra las gafas perfectas";
+    // Se cambia el texto del titulo
 
-    //se modifica a negrita el texto de deccribcion 
-    descripcion.innerHTML = "<strong>Calidad, estilo y comodidad</strong> en un solo lugar.";
+    titulo.textContent = "Encuentra las gafas perfectas";
+
+
+    // Se modifica el texto de la descripcion utilizando HTML
+
+    descripcion.innerHTML =
+        "<strong>Calidad, estilo y comodidad</strong> en un solo lugar.";
 
 });
 
 
 
-//modificacion de estilos 
-
-//se utliza el boton estilo (btnEtilo)
-
-//NOTA 
-// Buena practica:
-// classList suele ser mejor para aplicar estilos porque mantiene
-// separada la presentacion (CSS) de la logica (JavaScript).
-// style es util para cambios puntuales y dinamicos.
+// MODIFICACION DE ESTILOS
 
 
-btnEstilo.addEventLIstener("click", function(){
+/*
+    Buena practica:
 
+    classList suele ser mejor para aplicar estilos porque mantiene
+    separada la presentacion (CSS) de la logica (JavaScript).
+
+    style es util para cambios puntuales y dinamicos.
+*/
+
+
+btnEstilo.addEventListener("click", function () {
 
     // Cambio puntual usando style
-    //se cambia el tamnio 
-    titulo.style.frontSize = "40px";
 
-    
+    // Se cambia el tamano del titulo
 
-    // Manejo de una clase CSS
-    // se agrago destacado la lista de titulo 
-    titulo.classList.add("destacado");
-
-    console.log("se agrgo la clase destacando");
-
-});
+    titulo.style.fontSize = "40px";
 
 
-btnEstilo.addEventLIstener("click", function(){
+    // Alternar la clase CSS
 
-
-    // Eliminar la clase
-    // se elemina destacado la lista del titulo 
-    titulo.classList.remove("destacado");
-
-    console.log("se saco la clase destacado");
-
-});
-
-
-btnEstilo.addEventLIstener("click", function(){
-
-    // Alternar la clase
     titulo.classList.toggle("destacado");
 
-    console.log("se alterna  la clase destacado");
+    console.log("Se alterno la clase destacado");
+
+
+    //titulo.classList.remove("destacado");
+
+    //titulo.classList.toggle("destacado");
 
 });
 
-//CRUD DE GAFAS
 
 
-//Crear gafas dinamicamente
+// CREACION DINAMICA DE GAFAS
 
-//se crea una lista para almacenar echas gafas 
-let contanedorGafas;
+// Se crea un contador para las gafas
 
-// se crea una conatador para las gafas 
-let contadorGafas = 0; 
+let contadorGafas = 0;
 
 
-btnAgrgar.addEventLIstener("click", function() {
+btnAgregar.addEventListener("click", function () {
 
-    contadorGafas ++;
+    // Aumentamos el contador
 
-    const nuevaGafa = cocument.createElemnt("div");
+    contadorGafas++;
+
+
+    // Se crea un nuevo elemento div
+
+    const nuevaGafa = document.createElement("div");
+
+
+    // Se agrega la clase CSS producto
 
     nuevaGafa.classList.add("producto");
 
-    nuevaGafa.textContent = 
-        " gafa nueva #" + contadorGafas;
 
-    contanedorGafas.appendChild(nuevaGafa);
+    // Se agrega el texto
 
-    console.log("btn-Agrgar - Se creo:", nuevaGafa);
+    nuevaGafa.textContent =
+        "Gafa nueva #" + contadorGafas;
+
+
+    // Se agrega la nueva gafa al contenedor
+
+    contenedorGafas.appendChild(nuevaGafa);
+
+
+    console.log( "btnAgregar - Se creo:", nuevaGafa);
 
 });
 
-// se elimin a ell ultimo de la lista 
-btnEliminar.addEventLIstener("click", function(){
 
-    const ultimoGafa = contenedorGafas.lasElementChild;
+// ELIMINAR ULTIMA GAFA
 
 
-    if(ultimoGafa){
+btnEliminar.addEventListener("click", function () {
 
-        ultimoGafa.remove();
+    // Se obtiene el ultimo elemento del contenedor
 
-        console.log("se elmini ka ultima gafa");
+    const ultimaGafa =
+        contenedorGafas.lastElementChild;
+
+
+    // Comprobamos si existe una gafa
+
+    if (ultimaGafa) {
+
+        // Se elimina la ultima gafa
+
+        ultimaGafa.remove();
+
+        console.log("Se elimino la ultima gafa");
 
     } else {
 
-        console.log("no hay gafas para eleminar")
+        console.log("No hay gafas para eliminar");
+
     }
 
-} );
+});
+
